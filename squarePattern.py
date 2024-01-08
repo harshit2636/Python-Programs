5917f97c688e4b9eef612f2b9ed5caa808184f5e
Harshit Joshi <142739875+harshit2636@users.noreply.github.com>
2024-01-08 14:03:16 +0530

    Add files via upload
    Square Pattern

---
+++ squarePattern.py
@@ -0,0 +1,9 @@
+# Square pattern program
+size = 5
+
+# Create a list of rows
+for i in range(0, size):
+    # Create a list of columns
+    for j in range(0, size):
+        print("*", end="")
+    print()
